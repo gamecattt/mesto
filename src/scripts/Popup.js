b3bb7566bf8e2f26cc9f768b30c4e65d12baf425
gamecattt <ak19gamecat@gmail.com>
2022-11-23 22:07:27 +0300

    hot-fix

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -5,23 +5,19 @@ export class Popup {
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keyup', (event) => {
-      this._handleEscClose(event);
-    });
+    document.addEventListener('keyup', this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keyup', (event) => {
-      this._handleEscClose(event);
-    });
+    document.removeEventListener('keyup', this._handleEscClose);
   }
 
-  _handleEscClose(event) {
+  _handleEscClose = (event) => {
     if (event.key === 'Escape') {
       this.close();
     }
-  }
+  };
 
   setEventListeners() {
     this._popup.addEventListener('click', (event) => {
